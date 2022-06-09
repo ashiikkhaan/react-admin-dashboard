c5b7665e3aaa53d2a5e144a68b1ef2c11ee161dd
xavionashik <ashiksust26th@gmail.com>
2022-06-09 23:36:14 +0600

    set up done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <div>
+            <div className="home">Home</div>
+        </div>
+    );
+};
+
+export default Home;
